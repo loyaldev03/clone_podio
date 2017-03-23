d61d848c91c0873eecde3e01f33ec0daea29bdc5
john <johnmartin3387@gmail.com>
2017-03-22 20:11:35 -0700

    update dashboard with highchart

--- public/js/main.js
+++ public/js/main.js
@@ -118,12 +118,12 @@ MetronicApp.config(['$stateProvider', '$urlRouterProvider', function($stateProvi
                         name: 'MetronicApp',
                         insertBefore: '#ng_load_plugins_before', // load the above css files before a LINK element with this ID. Dynamic CSS files must be loaded between core and theme css files
                         files: [
-                            'assets/global/plugins/morris/morris.css',                            
-                            'assets/global/plugins/morris/morris.min.js',
-                            'assets/global/plugins/morris/raphael-min.js',                            
+                            'assets/global/plugins/highcharts/js/highcharts.js',
+                            'assets/global/plugins/highcharts/js/highcharts-3d.js',
+                            'assets/global/plugins/highcharts/js/highcharts-more.js',
                             'assets/global/plugins/jquery.sparkline.min.js',
 
-                            'assets/pages/scripts/dashboard.min.js',
+                            'assets/pages/scripts/dashboard.js',
                             'js/controllers/DashboardController.js',
                         ] 
                     });
