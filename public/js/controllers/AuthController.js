6bbf66c2552e8d87a0dd93d525db82037e168347
future21star <future21star@yahoo.com>
2017-12-21 16:09:39 -0500

    auth controller customization

--- public/js/controllers/AuthController.js
+++ public/js/controllers/AuthController.js
@@ -56,3 +56,61 @@ function($scope, $state, s_auth, $location, $stateParams, $auth){
       });    
   };
 }])
+angular.module('MetronicApp').controller('AuthController', [
+'$scope',
+'$state',
+'s_auth',
+'$location',
+'$stateParams',
+'$auth',
+function($scope, $state, s_auth, $location, $stateParams, $auth){
+  $scope.user = {};
+
+  $scope.register = function(){
+    s_auth.register($scope.user).error(function(error){
+      $scope.error = error;
+    }).then(function(){
+      $state.go( "dashboard" );
+    });
+  };
+
+  $scope.logIn = function(){
+    s_auth.logIn($scope.user).error(function(error){
+      $scope.error = error;
+    }).then(function(){
+      $state.go( "dashboard" );
+    });
+  };
+
+  $scope.logOut = function() {
+    s_auth.logOut($scope.user).error(function(error){
+      $scope.error = error;
+    }).then(function() {
+      $state.go( "login" );
+    })
+  }
+
+  $scope.createAccount = function() {
+    $state.go('register');
+  }
+  
+  $scope.authenticate = function(provider) {
+    $auth.authenticate(provider)
+      .then(function(access_token) {
+        console.log('You have successfully signed in with ' + provider + '!' + access_token);
+        $location.path('/');
+      })
+      .catch(function(error) {
+        console.log("---------------------error--------------------", error);
+        // if (error.message) {
+        //   // Satellizer promise reject error.
+        //   toastr.error(error.message);
+        // } else if (error.data) {
+        //   // HTTP response error from server
+        //   toastr.error(error.data.message, error.status);
+        // } else {
+        //   toastr.error(error);
+        // }
+      });    
+  };
+}])     
