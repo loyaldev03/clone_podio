5dc927da1371340e8a15feac25bb7622f9cdbff6
future21star <future21star@yahoo.com>
2017-12-22 20:21:22 -0500

    add property controller

--- public/js/controllers/PropertyController.js
+++ public/js/controllers/PropertyController.js
@@ -44,3 +44,51 @@ angular.module('MetronicApp').controller('PropertyController', [
     	$state.go('properties_new', {workspace_id: $stateParams.workspace_id, property_id: $stateParams.property_id});
     }
 }]);
+
+/* Setup Property page controller */
+angular.module('MetronicApp').controller('PropertyController', [
+	'$rootScope', 
+	'$scope', 
+	'settings', 
+	'$uibModal', 
+	'$log', 
+	's_property',
+	'$stateParams',
+	'$state',
+	function($rootScope, $scope, settings, $uibModal, $log, s_property, $stateParams, $state) {
+    $scope.$on('$viewContentLoaded', function() {   
+        // initialize core components
+        App.initAjax();
+
+        // set default layout mode
+        $rootScope.settings.layout.pageContentWhite = true;
+        $rootScope.settings.layout.pageBodySolid = false;
+        $rootScope.settings.layout.pageSidebarClosed = false;
+    });
+   
+   	$scope.newProperty = function() {
+   		$state.go('properties_new', {workspace_id: $stateParams.workspace_id});
+   	}
+   	//Fields for property
+    $scope.fields = s_property.fields;
+
+    $scope.property = {};
+    $scope.createProperty = function() {
+      s_property.create($scope.property, $stateParams.workspace_id).then(function(res){
+          // $state.go('properties_show', {workspace_id: $stateParams.workspace_id});
+          $state.go('properties_index', {workspace_id: $stateParams.workspace_id});
+      });
+    }
+
+    //All Properties for workspace
+    $scope.properties = s_property.properties;
+    
+    $scope.updateProperty = function() {
+    	$state.go('properties_show', {workspace_id: $stateParams.workspace_id, property_id: $stateParams.property_id});
+    }
+
+    $scope.addproperty = function() {
+    	$state.go('properties_new', {workspace_id: $stateParams.workspace_id, property_id: $stateParams.property_id});
+    }
+}]);
+
