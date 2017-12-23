a690bebdd20556098c9b8983b7559c863981d1e0
future21star <future21star@yahoo.com>
2017-12-22 20:19:31 -0500

    add todo controller

--- public/js/controllers/TodoController.js
+++ public/js/controllers/TodoController.js
@@ -8,3 +8,14 @@ angular.module('MetronicApp').controller('TodoController', function($rootScope, 
     $rootScope.settings.layout.pageBodySolid = true;
     $rootScope.settings.layout.pageSidebarClosed = true;
 });
+
+angular.module('MetronicApp').controller('TodoController', function($rootScope, $scope, $http, $timeout) {
+    $scope.$on('$viewContentLoaded', function() {   
+        App.initAjax(); // initialize core components        
+    });
+
+    // set sidebar closed and body solid layout mode
+    $rootScope.settings.layout.pageContentWhite = true;
+    $rootScope.settings.layout.pageBodySolid = true;
+    $rootScope.settings.layout.pageSidebarClosed = true;
+});
