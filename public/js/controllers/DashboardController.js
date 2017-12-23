9ec3b6c76b8c02165b9e9f615ad7be3b428368dc
future21star <future21star@yahoo.com>
2017-12-22 20:18:44 -0500

    load contents when initializing

--- public/js/controllers/DashboardController.js
+++ public/js/controllers/DashboardController.js
@@ -5,6 +5,12 @@ angular.module('MetronicApp').controller('DashboardController', function($rootSc
 
     });
 
+    $scope.$on('$viewContentLoaded', function() {   
+        // initialize core components
+        App.initAjax();
+
+    });
+    
     // set sidebar closed and body solid layout mode
     $rootScope.settings.layout.pageContentWhite = true;
     $rootScope.settings.layout.pageBodySolid = false;
