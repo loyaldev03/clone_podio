95f32f1ff0efe4b4b8c34649a19c371a0b8465db
future21star <future21star@yahoo.com>
2017-12-23 19:59:27 -0500

    workspace controller customization

--- controllers/workspace.controller.js
+++ controllers/workspace.controller.js
@@ -50,3 +50,56 @@ exports.createDefaultWorkspace = function(user){
     });
   });
 };
+
+var express = require('express');
+var router = express.Router();
+var mongoose = require('mongoose');
+var User = mongoose.model('User');  
+var Workspace = mongoose.model('Workspace');  
+var Appp = mongoose.model('Appp');  
+var Item = mongoose.model('Item');  
+var Field = mongoose.model('Field');  
+var Value = mongoose.model('Value');  
+
+//import controllers
+var appp_controller = require('./appp.controller');
+var field_controller = require('./field.controller');
+
+var exports = module.exports; 
+
+exports.createDefaultWorkspace = function(user){
+  return new Promise(function(resolve, reject) {
+    var username = "unnamed";
+    if (user.username) {
+      username = user.username;
+    }
+    else if (user.facebook) {
+      username = user.facebook.name;
+    }
+    else {
+      username = user.twitter.username;
+    }
+    var workspace = new Workspace({
+      title: username+"'s leads",
+      description: "default workspace",
+      user: user._id
+    });
+    workspace.save(function(err, workspace) {
+      if (err) {
+        return reject(err);
+      }
+      User.update({_id: workspace.user}, {$addToSet: {workspaces: workspace._id}, default_workspace: workspace._id}, function(err, user) {
+        if (err) {
+          return reject(err);
+        }
+        appp_controller.createDefaultAppp(workspace)
+        .then(function(appp) {
+          return resolve(workspace);
+        })
+        .catch(function(err) {
+          return reject(err);
+        });  
+      });
+    });
+  });
+};
