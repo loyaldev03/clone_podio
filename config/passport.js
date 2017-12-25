07a43892b68e0a54f252881f54333e46a859efce
future21star <future21star@yahoo.com>
2017-12-25 18:33:59 -0500

    adding token-check module for authentication

--- config/passport.js
+++ config/passport.js
@@ -166,6 +166,51 @@ passport.use('facebook', new FacebookStrategy({
 
   }
 							      
+  function(token, refreshToken, profile, done) {
+      // asynchronous
+      process.nextTick(function() {
+          // find the user in the database based on their facebook id
+          User.findOne({ 'facebook.id' : profile.id }, function(err, user) {
+              // if there is an error, stop everything and return that
+              // ie an error connecting to the database
+              if (err) {
+                return done(err);
+              }
+              // if the user is found, then log them in
+              if (user) {
+                  return done(null, user); // user found, return that user
+              } else {
+                  // if there is no user found with that facebook id, create them
+                  var newUser = new User();
+                  // set all of the facebook information in our user model
+                  var facebook = {};
+                  facebook.id    = profile.id; // set the users facebook id                   
+                  facebook.token = token; // we will save the token that facebook provides to the user                    
+                  facebook.name  = profile.name.givenName + ' ' + profile.name.familyName; // look at the passport user profile to see how names are returned
+                  facebook.email = profile.emails[0].value; // facebook can return multiple emails so we'll take the first
+                  newUser.facebook = facebook;
+                  newUser.activated = true;
+                  // save our user to the database
+                  newUser.save(function(err) {
+                      if (err)
+                          throw err;
+                      // if successful, create default workspace and return user
+											workspace_controller.createDefaultWorkspace(newUser)
+													.then(function(workspace){
+			                      return done(null, newUser);
+													})
+													.catch(function(err) {
+														return done(err);
+													})                      
+                  });
+              }
+
+          });
+      });
+
+  }
+							      
+
 ));
 
 passport.use(new TwitterStrategy({
