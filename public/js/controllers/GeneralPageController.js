f88e84f8fed07d43c9e3352a1ac498c5585dcb6a
future21star <future21star@yahoo.com>
2017-12-25 18:42:56 -0500

    general page controller replication

--- public/js/controllers/GeneralPageController.js
+++ public/js/controllers/GeneralPageController.js
@@ -8,5 +8,9 @@ angular.module('MetronicApp').controller('GeneralPageController', ['$rootScope',
     	$rootScope.settings.layout.pageContentWhite = true;
         $rootScope.settings.layout.pageBodySolid = false;
         $rootScope.settings.layout.pageSidebarClosed = false;
+    	// set default layout mode
+    	$rootScope.settings.layout.pageContentWhite = true;
+        $rootScope.settings.layout.pageBodySolid = false;
+        $rootScope.settings.layout.pageSidebarClosed = false;        
     });
 }]);
