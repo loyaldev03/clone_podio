bd5f00946a9b6b4254239a8005d686f5385d8b1b
future21star <future21star@yahoo.com>
2017-12-27 15:30:25 -0500

    is logged in service for authentication

--- public/js/services/s_auth.js
+++ public/js/services/s_auth.js
@@ -92,6 +92,17 @@ angular.module('MetronicApp')
 		}
 	}
 
+	auth.isLoggedIn = function() {
+		var token = auth.getToken();
+
+		if (token) {
+			var payload = JSON.parse($window.atob(token.split('.')[1]));
+			return payload.exp > Date.now() / 1000;
+		} else {
+			return false;
+		}
+	}	
+
 	auth.currentUser = function() {
 		if (auth.isLoggedIn()) {
 			var token = auth.getToken();
