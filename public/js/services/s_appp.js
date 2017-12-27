e1d4120c23dd077dad8948d7120c68b4f5e3f25f
future21star <future21star@yahoo.com>
2017-12-27 15:26:41 -0500

    appp service creation

--- public/js/services/s_appp.js
+++ public/js/services/s_appp.js
@@ -43,3 +43,49 @@ angular.module('MetronicApp')
 		// }
 		return appp;
 }]);
+
+angular.module('MetronicApp')
+.factory('s_appp', [
+	'$http', 
+	'$window', 
+	's_auth', 
+	function($http, $window, s_auth){
+		var appp = {
+			all_appps: [],
+			current_appp: {}
+		};
+
+		appp.create = function(appp) {
+			return $http.post('/api/v1/appps', appp, {
+				headers: {Authorization: 'Bearer '+s_auth.getToken()}
+			}).success(function(appp){
+						return appp;
+			})
+			}
+		appp.get = function(id) {
+			return $http.get('/api/v1/appp/'+id, {
+				headers: {Authorization: 'Bearer '+s_auth.getToken()}
+			}).then(function(res){
+					return res.data;
+			})
+		}
+		appp.setCurrentAppp = function(id) {
+			return $http.get('/api/v1/appp/'+id, {
+				headers: {Authorization: 'Bearer '+s_auth.getToken()}
+			}).then(function(res){
+					appp.current_appp = res.data;
+					return res.data;
+			})
+		}
+		appp.getCurrentAppp = function() {
+			return appp.current_appp;
+		}
+		// appp.getAllAppps = function() {
+		// 	return $http.get('/api/v1/appps', {
+		// 		headers: {s_authorization: 'Bearer ' + s_auth.getToken()}
+		// 	}).then(function(res) {
+		// 			return res.data;
+		// 	})
+		// }
+		return appp;
+}]);		
